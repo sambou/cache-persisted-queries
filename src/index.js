bc20207b3ad6bcc61c2363e731193a6f0a8ebcd9
Sami <sboussaid3@gmail.com>
2018-03-07 17:00:45 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+const apicache = require('apicache');
+apicache.options({ debug: true, enabled: process.env.CACHE === 'true' });
+
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+// The GraphQL schema in string form
+const typeDefs = /* GraphQL */ `
+  type Query {
+    books: [Book]
+  }
+
+  type Book {
+    title: String
+    author: String
+  }
+`;
+
+// The resolvers
+const resolvers = {
+  Query: {
+    // Simulate slow data access
+    books: () => new Promise(resolve => setTimeout(() => resolve(books), 500)),
+  },
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// A map from query id -> query text
+// How this is created and persisted depends on your app
+const queryMap = {
+  a: /* GraphQL */ `query A {
+    books {
+      title
+      author
+    }
+  }`,
+  b: /* GraphQL */ `query B {
+    books {
+      author
+    }
+  }`,
+};
+
+// server-side lookup middleware for finding query id -> query text
+function persistedQueryParser(req, res, next) {
+  if (req.query.id) {
+    req.query.query = queryMap[req.query.id];
+  }
+  next();
+}
+
+// Initialize the app
+const app = express();
+
+// The GraphQL endpoint
+app.use(
+  '/graphql',
+  // in-memory cache for demo -- in production this could be done via CDN
+  apicache.middleware('60 seconds'),
+  // register our lookup middleware
+  persistedQueryParser,
+  graphqlExpress({ schema }),
+);
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Go to http://localhost:3000/graphiql to run queries!');
+});
